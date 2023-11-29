7857ad8d3af1fa4150dda4deed767be6463e2853
HUTZAKI <99677673+HUTZAKI@users.noreply.github.com>
2023-11-29 17:12:34 +0000

    Homework1.py

--- Homework1.py
+++ Homework1.py
@@ -1,6 +1,6 @@
 import turtle
 
-
+# create a unique petal
 def create_petal(n):
   a,b,c,d =  2,3,3,3
   if n == 16:
@@ -22,6 +22,7 @@ def create_petal(n):
       turtle.right(c)
       turtle.forward(d)
 
+#create a mixed flower
 def create_flower():
   for i in range(12):
     if (i % 2) == 0:
@@ -32,6 +33,10 @@ def create_flower():
       create_petal(16)
 
 
+#parameter: "angle_st" is the first move that resets the old position.
+#parameter: "type_" sets the direction of the curve.
+#parameter: "angle_th" is the rate at which the angle changes during forward motion.
+
 def create_flower_stem(angle_st,type_,angle_th,long):
   turtle.left(angle_st)
   for i in range(12):
@@ -41,25 +46,30 @@ def create_flower_stem(angle_st,type_,angle_th,long):
       turtle.left(angle_th)
     turtle.forward(long)
 
+# create a flower type1
 def create_flower_t1():
   for i in range(16):
     turtle.left(8)
     create_petal(15)
-
+#create a flower type2
 def create_flower_t2():
     for i in range(14):
       turtle.left(8)
       create_petal(16)
 
-create_flower()
-create_flower_stem(145,"r",2,20)
-create_flower_stem(200,"l",3,30)
-create_flower_t1()
-turtle.penup()
-turtle.left(203)
-turtle.forward(355)
-turtle.pendown()
-create_flower_stem(110,"l",2.8,32)
-create_flower_t2()
-turtle.left()
-turtle.mainloop()
+if __name__ == "__main__":
+  create_flower()  #create flower mix together between t1 and t2
+  create_flower_stem(145,"r",2,20) #function for create a stem 1
+  create_flower_stem(200,"l",3,30) #function for create a stem 2
+  create_flower_t1() #create flower t2
+
+  turtle.penup() #reset a pen
+  turtle.left(203)
+  turtle.forward(355) #move the cursor to start point
+  turtle.pendown() #wirte
+  
+  create_flower_stem(110,"l",2.8,32) #create a stem 3
+  create_flower_t2() #create a flower type 3
+  turtle.left()
+
+  turtle.mainloop() #show slide
